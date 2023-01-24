3da998bd91647448d039539a7b121dba3b09d8dd
Luis Florencio Meneses Flores <luis_menesesf07@hotmail.com>
2023-01-24 17:30:34 -0600

    cards and content main

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import {
   RiUser3Line,
   RiCloseLine,
   RiSearch2Line,
-  RiArrowDownSLine
+  RiArrowDownSLine,
 } from "react-icons/ri";
+import Card from "./components/shared/Card";
 function App() {
   const [showMenu, setShowMenu] = useState(false);
   const [showOrden, setShowOrden] = useState(false);
@@ -35,9 +36,9 @@ function App() {
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </nav>
-      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
-        <div className="lg:col-span-6">
-          <header className="p-4">
+      <main className="lg:pl-32 grid grid-cols-1 lg:grid-cols-8 p-4 pb-20">
+        <div className="lg:col-span-6 md:p-8">
+          <header className="">
             {/* Title and search */}
             <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
               <div>
@@ -48,7 +49,8 @@ function App() {
                 <div className="w-full relative">
                   <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300" />
                   <input
-                    type="text" placeholder="Search"
+                    type="text"
+                    placeholder="Search"
                     className="bg-[#1F1D2B] w-full py-2 pl-8 pr-4 rounded-lg text-gray-300 outline-none"
                   />
                 </div>
@@ -56,17 +58,50 @@ function App() {
             </div>
             {/* Tabs*/}
             <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
-              <a href="" className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]">Hot Dishes</a>
-              <a href="" className="py-2 pr-4">Cold Dishes</a>
-              <a href="" className="py-2 pr-4">Soup</a>
-              <a href="" className="py-2">Grill</a>
+              <a
+                href=""
+                className="relative py-2 pr-4 before:w-1/2 before:h-[2px] before:absolute before:bg-[#ec7c6a] before:left-0 before:rounded-full before:-bottom-[1px] text-[#ec7c6a]"
+              >
+                Hot Dishes
+              </a>
+              <a href="" className="py-2 pr-4">
+                Cold Dishes
+              </a>
+              <a href="" className="py-2 pr-4">
+                Soup
+              </a>
+              <a href="" className="py-2">
+                Grill
+              </a>
             </nav>
-            {/* Title content */}
-            <div className="flex items-center justify-between">
-              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-              <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg"><RiArrowDownSLine/>Dine in</button>
+          </header>
+          {/* Title content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine />
+              Dine in
+            </button>
+          </div>
+          {/*content*/}
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+            {/* Card */}
+            <div className="bg-[#1F1D2B] py-8 px-10 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center">
+              <img
+                src="comida.png"
+                alt="plato comida"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-2xl">Speacy sead</p>
+              <span>$2.29</span>
+              <p>20 comidas</p>
             </div>
-          </header> 
+            <Card img="comida.png" price="2.30" description="plato con comida"/>
+            <Card img="comida.png" price="2.30" description="plato con comida"/>
+            <Card img="comida.png" price="2.30" description="plato con comida"/>
+            <Card img="comida.png" price="2.30" description="plato con comida"/>
+            <Card img="comida.png" price="2.30" description="plato con comida"/>
+          </div>
         </div>
         <div className="lg:col-span-2 fixed lg:static right-0">Hola 2</div>
       </main>
---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  
+} from "react-icons/ri";
+
+const Card = (props) => {
+
+  return (
+    <div className="bg-[#1F1D2B] py-8 px-10 rounded-xl flex flex-col items-center gap-2 text-gray-300 text-center">
+              <img
+                src={props.img}
+                alt="plato comida"
+                className="w-40 h-40 object-cover -mt-20 shadow-2xl rounded-full"
+              />
+              <p className="text-2xl">Speacy sead</p>
+              <span>{props.price}</span>
+              <p>{props.description}</p>
+            </div>
+  );
+};
+
+export default Card;
